d38efcb4b7ac9c1203c0956246ae4dac13086610
Almat Kairatov <92521753+almatkai@users.noreply.github.com>
2024-07-10 18:30:07 +0500

    Bug fix

--- AgainUIKIT/Controllers/Core/RMTabViewController.swift
+++ AgainUIKIT/Controllers/Core/RMTabViewController.swift
@@ -19,26 +19,26 @@ final class RMTabViewController: UITabBarController {
     private func setupTabs() {
         /// Init vcs
         let characterVC = RMCharacterViewController()
-        let settingsVC = RMSettingsViewController()
         let episodeVC = RMEpisodeViewController()
         let locationVC = RMLocationViewController()
+        let settingsVC = RMSettingsViewController()
         
-        /// Init UINavigationController 
+        /// Init UINavigationController
         let characterNav = UINavigationController(rootViewController: characterVC)
-        let settingsNav = UINavigationController(rootViewController: settingsVC)
         let episodeNav = UINavigationController(rootViewController: episodeVC)
         let locationNav = UINavigationController(rootViewController: locationVC)
+        let settingsNav = UINavigationController(rootViewController: settingsVC)
     
         /// Init UITabBarItem
         characterNav.tabBarItem = UITabBarItem(title: "Character", image: UIImage(systemName: "person"), tag: 0)
-        settingsNav.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 1)
         episodeNav.tabBarItem = UITabBarItem(title: "Episode", image: UIImage(systemName: "tv"), tag: 2)
         locationNav.tabBarItem = UITabBarItem(title: "Location", image: UIImage(systemName: "location"), tag: 3)
+        settingsNav.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 1)
         
-        for nav in [characterNav, settingsNav, episodeNav, locationNav]{
+        for nav in [characterNav, episodeNav, locationNav, settingsNav]{
             nav.navigationBar.prefersLargeTitles = true
         }
         tabBar.tintColor = .label
-        setViewControllers([characterNav, settingsNav, episodeNav, locationNav], animated: true)
+        setViewControllers([characterNav, episodeNav, locationNav, settingsNav], animated: true)
     }
 }
